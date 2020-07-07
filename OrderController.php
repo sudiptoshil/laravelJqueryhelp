8fdbf5299125c579c71facea906c3f436b3f5070
sudipto <sudiptoshil@outlook.com>
2020-07-07 17:36:58 +0600

    2nd migration

--- OrderController.php
+++ OrderController.php
@@ -1,120 +1,112 @@
 <?php
 
-namespace App\Http\Controllers\Client;
+namespace App\Http\Controllers\Admin\Order;
 
 use App\Http\Controllers\Controller;
-use App\model\Invoice\Invoice;
-use App\model\Product\Product;
-use Illuminate\Http\Request;
-use Cart;
 use App\model\Client\Order;
-use App\model\Client\payment;
-use Session;
-use Mail;
+use App\model\Invoice\Invoice;
+use Illuminate\Http\Request;
 use DB;
+
 class OrderController extends Controller
 {
-    public function confirm_order(Request $request)
+
+    public function manageOrder()
     {
-        // $ordertotal = Cart::priceTotal();
-
-        if (Session::get('totalAfterDiscount')){
-
-            $order = new Order();
-            $order->first_name   = $request->name;
-            $order->last_name    = $request->lastname;
-            $order->email        = $request->email;
-            $order->status       = $request->status;
-            $order->address      = $request->address;
-            $order->phone_number = $request->phonenumber;
-            $order->post_code    = $request->postcode;
-            $order->client_id    = Session::get('client_id');
-            $order->sub_total    = (float)(Session::get('discount'))+(float)(Session::get('totalAfterDiscount'));
-            $order->discount    = Session::get('discount');
-            $order->total_cost   = Session::get('totalAfterDiscount');
-
-
-            $order->save();
-
-        }
-
-        else{
-            $order = new Order();
-            $order->first_name   = $request->name;
-            $order->last_name    = $request->lastname;
-            $order->email        = $request->email;
-            $order->status       = $request->status;
-            $order->address      = $request->address;
-            $order->phone_number = $request->phonenumber;
-            $order->post_code    = $request->postcode;
-            $order->client_id    = Session::get('client_id');
-            $order->sub_total    = Session::get('ordertotal');
-            $order->discount    = 0;
-            $order->total_cost   = Session::get('ordertotal');
-
-
-            $order->save();
-        }
-
-        /*$order = new Order();
-        $order->first_name   = $request->name;
-        $order->last_name    = $request->lastname;
-        $order->email        = $request->email;
-        $order->status       = $request->status;
-        $order->address      = $request->address;
-        $order->phone_number = $request->phonenumber;
-        $order->post_code    = $request->postcode;
-        $order->client_id    = Session::get('client_id');
-        $order->total_cost   = Session::get('totalAfterDiscount');*/
-
-
-        //$order->save();
-
-        $cartproduct = Cart::content();
-        foreach ($cartproduct as $v_cartProduct) {
-            $pInfo = Product::find($v_cartProduct->id);
-
-
-            DB::table("products")->where('id', $pInfo->id)->increment('popularity');
-
-
-            $invoice = new Invoice();
-            $invoice->vendor_id  = $pInfo->vendor->id;
-            $invoice->order_id   = $order->id;
-            $invoice->client_id  = Session::get('client_id');
-            $invoice->product_id = $v_cartProduct->id;
-            $invoice->product_unit_price = $v_cartProduct->price;
-            $invoice->product_quantity   = $v_cartProduct->qty;
-            $unit_x_quantity = $v_cartProduct->price * $v_cartProduct->qty;
-            $invoice->unit_x_quantity   = $unit_x_quantity;
-
-            $total_discount = $pInfo->discount*$v_cartProduct->qty;
-            $invoice->invoice_discount   = $total_discount;
-            $invoice->invoice_after_discount   = $unit_x_quantity - $total_discount;
-            $invoice->save();
-        }
-        $payment = new payment();
-        $payment->order_id = $order->id;
-        $payment->payment_type = $request->payment;
-        $payment->client_id   = Session::get('client_id');
-        $payment->card_name   = $request->cardname;
-        $payment->card_number = $request->cardno;
-        $payment->expireDate  = $request->expireDate;
-        $payment->security_number = $request->security_number;
-        $payment->comment = $request->comment;
-        $payment->save();
-        Cart::destroy();
-        Session::forget('discount');
-        Session::forget('totalAfterDiscount');
-
-
-        $data = $order->toArray();
-         Mail::send('Client.Emails.order_create',$data,function($message) use ($data){
-             $message->to($data['email']);
-             $message->subject('order success email');
-         });
-        return "success!!";
-        // return $request->all();
-        //    return redirect('/');
+        $orders = DB::table('orders')
+            ->join('clients', 'orders.client_id', '=', 'clients.id')
+            ->select('orders.*', 'clients.full_name', 'clients.contact_no')
+            ->orderBy('id','desc')
+            ->get();
+        return view('Admin.Orders.manage-orders',[
+            'orders'=> $orders
+        ]);
     }
+
+    public function orderDetails($id){
+        $orders = DB::table('invoices')
+            ->join('orders', 'invoices.order_id', '=', 'orders.id')
+            ->join('clients', 'orders.client_id', '=', 'clients.id')
+            ->join('products', 'invoices.product_id', '=', 'products.id')
+            ->join('vendor_auths', 'invoices.vendor_id', '=', 'vendor_auths.id')
+            ->select('invoices.*', 'clients.full_name','products.product_name','vendor_auths.vendor_name','vendor_auths.phone')
+            ->where('invoices.order_id',$id)
+            ->get();
+
+        $clientDetails = DB::table('orders')
+            ->join('clients', 'orders.client_id', '=', 'clients.id')
+            ->select('clients.*','orders.id', 'orders.first_name', 'orders.last_name', 'orders.email', 'orders.address', 'orders.phone_number','orders.sub_total','orders.discount','orders.total_cost')
+            ->where('orders.id',$id)
+            ->get();
+
+        return view('Admin.Orders.order-details',[
+            'orders' => $orders,
+            'clientDetails' => $clientDetails,
+        ]);
+    }
+
+    public function acceptOrder($id){
+        
+        $invoice = Invoice::find($id);
+        $invoice->status = 4;
+        $invoice->save();
+        return back();
+    }
+
+    public function cancelOrder($id){
+        
+        $invoice = Invoice::find($id);
+        $invoice->status = 3;
+        $invoice->save();
+        return back();
+    }
+
+    public function deleteOrder($id){
+       
+        $orders = Order::find($id);
+        $orders->delete();
+
+        $invoice = Invoice::where('order_id',$id);
+        $invoice->delete();
+        return back();
+    }
+    
+    public function order_search_filter(Request $request)
+    {
+        // return $request->all();
+        // exit();
+        // $date="";$status="";$expected_date="";
+        // if(!empty($request->get('date'))){
+        //     $date   = $request->get('date');
+        // }
+        // if(!empty($request->get('status'))){
+        //     $status = $request->get('status');
+        // }
+        // if(!empty($request->get('exdate'))){
+        //     $expected_date = $request->get('exdate');
+        // }
+        
+        $from = $request->get('date');
+        $to =  $request->get('exdate');
+        $status = $request->get('status');
+        
+        $orders = Order::whereBetween('created_at', [$from, $to])->where('status','=',$status)->get();
+        
+        
+        //  $orders = Order::query()
+        //     ->where(function ($filter) use ($date,$status) {
+        //         if (!empty($date))
+        //             $filter->where('created_at', 'LIKE', $date);
+        //         if (!empty($expected_date))
+        //             $filter->where('created_at', 'LIKE', $expected_date);
+        //         if (!empty($status))
+        //             $filter->where('status', 'LIKE', $status);
+        //     })->get();
+            
+        // return $orders;
+        return view('Admin.Orders.order_result',[
+          'orders' => $orders
+        ]);
+    }
+
 }
